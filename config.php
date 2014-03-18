be3eabf145b717c6312140461f98b84aa8986f43
Ygor <ygor@Ygor-Inspiron-N4110.(none)>
2014-03-18 14:43:50 -0300

    alteração do nome do database.

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 	$config = array();
 	$config["dbhost"] = "localhost";
-	$config["dbname"] = "ufpbdb";
+	$config["dbname"] = "bicho";
 	$config["dbuser"] = "postgres";
 	$config["dbpass"] = "postgres";
 ?>
