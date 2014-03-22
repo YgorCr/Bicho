405748231a5520d50f1c1a8e489c490fcfcda27e
Ygor <ygor@Ygor-Inspiron-N4110.(none)>
2014-03-22 19:55:43 -0300

    criação da table jogador

---
+++ etc/migrations/bd_22-03-2014-19h52.sql
@@ -0,0 +1,13 @@
+create sequence jogador_id_seq start with 1;
+
+create table jogador(
+id integer default nextval('jogador_id_seq') primary key,
+nome varchar not null,
+cpf_cod varchar not null,
+estador char(2) not null,
+cidade varchar not null,
+rua varchar not null,
+bairro varchar not null,
+complemento varchar,
+senha char(32) not null
+);
